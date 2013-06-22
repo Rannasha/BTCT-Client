2527e40ff722ea32195fbc82d65c0e2b9c87ef48
Gideon Wormeester <gideon_g@wormeester.nl>
2013-06-22 12:14:19 +0200

    First commit

---
+++ BTCT-Client/BTCTCUtils.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using OAuth;
+using Newtonsoft.Json.Linq;
+
+namespace BTCTC
+{
+    public enum OrderType { OT_SELL, OT_BUY, OT_TIN, OT_TOUT };
+
+    [Serializable]
+    public class BTCTException : System.Exception
+    {
+        public BTCTException() : base() { }
+        public BTCTException(string message) : base(message) { }
+        public BTCTException(string message, System.Exception inner) : base(message, inner) { }
+
+        protected BTCTException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) { }
+    }
+
+    public class BTCTLink
+    {
+        private string _consumerKey;
+        private string _consumerSecret;
+        private const string _tradeUrl = "https://btct.co/oauth/trade";
+
+        private OAuthConfig _oauthConfig;
+        private OAuthConsumer _oauthConsumer;
+
+        public BTCTLink(string consumerKey, string consumerSecret)
+        {
+            _consumerKey = consumerKey;
+            _consumerSecret = consumerSecret;
+            
+            _oauthConfig = new OAuthConfig("");
+            _oauthConfig.SiteUrl = "";
+            _oauthConfig.OauthVersion = "1.0";
+            _oauthConfig.OauthSignatureMethod = "HMAC-SHA1";
+            _oauthConfig.ConsumerKey = _consumerKey;
+            _oauthConfig.ConsumerSecret = _consumerSecret;
+            _oauthConfig.RequestTokenUrl = "https://btct.co/oauth/request_token";
+            _oauthConfig.AccessTokenUrl = "https://btct.co/oauth/access_token";
+            _oauthConfig.UserAuthorizationUrl = "https://btct.co/authorize";
+
+            _oauthConsumer = new OAuthConsumer(_oauthConfig, "");
+        }
+
+        public void getRequestToken()
+        {
+            try
+            {
+                _oauthConsumer.getRequestToken();
+            }
+            catch (Exception e)
+            {
+                throw new BTCTException("Unable to get request token.");
+            }
+        }
+
+        public void getAccessToken(string verifier)
+        {
+            try
+            {
+                _oauthConsumer.getAccessToken(verifier);
+            }
+            catch (Exception e)
+            {
+                throw new BTCTException("Unable to get access token.");
+            }
+        }
+
+        private string rawRequest(List<QueryParameter> p)
+        {
+            string response = (string)_oauthConsumer.request(_tradeUrl, "POST", p, "PLAIN");
+
+            return response;
+        }
+
+        public Portfolio parsePortfolio(string json)
+        {
+            JObject r = JObject.Parse(json);
+            Portfolio pf = new Portfolio();
+
+            pf.username = (string)r["username"];
+            string st = (string)r["generated"];
+            string[] formats = { "MM/dd/yyyy HH:mm:ss" };
+            pf.lastUpdate = DateTime.ParseExact(st, formats, new CultureInfo("en-US"), DateTimeStyles.None);
+
+            List<SecurityOwned> SOList = new List<SecurityOwned>();
+            foreach (JProperty c in r["securities"].Children())
+            {
+                Security s = new Security();
+                s.name = (string)c.Name;
+                int a = Convert.ToInt32((string)c.First["quantity"]);
+                SecurityOwned so = new SecurityOwned(s, a);
+                SOList.Add(so);
+            }
+
+            pf.securities = SOList;
+
+            return pf;
+        }
+
+        public Portfolio getPortfolio()
+        {
+            List<QueryParameter> p = new List<QueryParameter>();
+            p.Add(new QueryParameter("act","get_portfolio"));
+            string response = rawRequest(p);
+
+            Portfolio pf = parsePortfolio(response);
+
+            return pf;
+        }
+    }
+
+    public class Security
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Order
+    {
+        public int id { get; set; }
+        public Security security { get; set; }
+        public int amount { get; set; }
+        public long price { get; set; }
+        public OrderType orderType { get; set; }
+        public bool active { get; set; }
+        public DateTime dateTime { get; set; }
+    }
+
+    public class SecurityOwned
+    {
+        public Security security { get; set; }
+        public int amount { get; set; }
+
+        public SecurityOwned(Security s, int a)
+        {
+            security = s;
+            amount = a;
+        }
+    }
+    
+    public class Portfolio
+    {
+        public List<SecurityOwned> securities { get; set; }
+        public List<Order> orders { get; set; }
+        public DateTime lastUpdate { get; set; }
+        public long balance { get; set; }
+        public string username { get; set; }
+    }
+
+    public class TradeHistory
+    {
+        public List<Order> orders { get; set; }
+    }
+
+}
---
+++ BTCT-Client/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using OAuth;
+
+namespace BTCTC
+{
+    public partial class Form1 : Form
+    {
+        private const string _consumerKey = "20bd6751441ff12b98117f4be1c09a9371de4cf7";
+        private const string _consumerSecret = "0949565dac0d493501a84cbab79a0f9eb6c936a9";
+        private BTCTLink b;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b = new BTCTLink(_consumerKey, _consumerSecret);
+//            b.getRequestToken();
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            b.getAccessToken(textBox3.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        //    Portfolio p = b.getPortfolio();
+            Portfolio p = b.parsePortfolio(textBox1.Text);
+
+            textBox4.Text = "User: " + p.username + Environment.NewLine;
+            textBox4.Text += "Generated: " + p.lastUpdate.ToString() + Environment.NewLine;
+            foreach (SecurityOwned so in p.securities)
+            {
+                textBox4.Text += so.security.name + "(" + Convert.ToString(so.amount) + ")" + Environment.NewLine;
+            }
+        }
+
+
+    }
+}
